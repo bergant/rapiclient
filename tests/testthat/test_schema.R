60cef9a7f0bd9991cf4c985b1857188506152600
LiNk-NY <marcel.ramosperez@roswellpark.org>
2024-06-07 15:25:44 -0400

    add more tests

--- tests/testthat/test_schema.R
+++ tests/testthat/test_schema.R
@@ -3,16 +3,17 @@ test_that("schema functions work", {
         id = list(type = "integer", format = "int64"),
         name = list(type = "string", example = "doggie")
     )
-    expect_identical(
-        get_schema(
-            list(
-                definitions = list(
-                    Pet = list(properties = pet_properties)
-                )
-            ),
-            "Pet",
-            compose_allOf = TRUE
+    schema <- get_schema(
+        list(
+            definitions = list(
+                Pet = list(properties = pet_properties)
+            )
         ),
+        "Pet",
+        compose_allOf = TRUE
+    )
+    expect_identical(
+        schema,
         structure(
             list(properties = pet_properties),
             name = "Pet",
@@ -29,4 +30,81 @@ test_that("schema functions work", {
         ),
         structure(list(), .Names = character(0L))
     )
+
+    animal_properties <- list(
+        name = list(type = "string", example = "dog")
+    )
+    schema <- get_schema(
+        list(
+            definitions = list(
+                Pet = list(
+                    allOf = list(
+                        list(`$ref` = "#/definitions/Animal")
+                    )
+                ),
+                Animal = list(properties =  animal_properties)
+            )
+        ),
+        "Pet",
+        compose_allOf = TRUE
+    )
+    expect_identical(
+        schema$properties, animal_properties
+    )
+    expect_identical(
+        schema$type, "object"
+    )
+
+    schema <- get_schema(
+        list(
+            definitions = list(
+                Pet = list(
+                    allOf = list(
+                        list(`$ref` = "#/definitions/Animal")
+                    ),
+                    type = "household"
+                ),
+                Animal = list(properties =  animal_properties)
+            )
+        ),
+        "Pet",
+        compose_allOf = TRUE
+    )
+    expect_identical(
+        schema$properties, animal_properties
+    )
+    expect_identical(
+        schema$type, "household"
+    )
+
+    schema <- get_schema(
+        list(
+            definitions = list(
+                House_Pet = list(properties = pet_properties)
+            )
+        ),
+        "#/definitions/House Pet",
+        compose_allOf = TRUE
+    )
+    expect_identical(
+        schema,
+        structure(
+            list(properties = pet_properties),
+            name = "House_Pet",
+            class = c("rapi_schema", "list")
+        )
+    )
+
+    api <- list(definitions = list(
+        Pet = list(
+            allOf = list(list( properties = animal_properties ))
+        ),
+        Animal = list(properties =  animal_properties)
+    ))
+    schema <- get_schema(api, "Pet", compose_allOf = TRUE)
+    expect_identical(
+        get_allOf(api, schema$allOf),
+        animal_properties
+    )
+
 })
